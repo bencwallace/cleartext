fixed token masking

cleartext/models/encoder_decoder.py
@@ -57,11 +57,12 @@ class Attention(nn.Module):
         super().__init__()
         self.attn_in = 2 * enc_units + dec_units
 
-        self.fc1 = nn.Linear(self.attn_in, units)
+        self.fc = nn.Linear(self.attn_in, units)
         self.fc2 = nn.Linear(units, 1)
         self.dropout = nn.Dropout(dropout)
 
-        utils.init_weights_(self.fc1)
+        utils.init_weights_(self.fc)
+        utils.init_weights_(self.fc2)
 
     def forward(self, dec_state: Tensor, enc_outputs: Tensor) -> Tensor:
         """
@@ -79,9 +80,8 @@ class Attention(nn.Module):
         enc_outputs = enc_outputs.permute(1, 0, 2)
         combined = torch.cat((dec_state, enc_outputs), dim=2)
         combined = self.dropout(combined)
-        scores = torch.tanh(self.fc1(combined))                                          # (len, batch, units)
+        scores = torch.tanh(self.fc(combined))                                          # (len, batch, units)
         scores = self.fc2(scores).squeeze(-1)
-        # scores = torch.tanh(self.fc(combined))
         # scores = torch.sum(scores, dim=2)
 
         weights = softmax(scores, dim=1)
@@ -224,7 +224,7 @@ class EncoderDecoder(nn.Module):
                 # apply softmax
                 probs = softmax(out, dim=1)                                     # (1, vocab_size)
                 # mask out <unk>
-                probs[trg_unk] = 0
+                probs[0, trg_unk] = 0
 
                 # update scores
                 curr_score = scores[i].item()

cleartext/pipeline/pipeline.py
@@ -1,3 +1,4 @@
+import random
 import time
 import warnings
 from pathlib import Path
@@ -113,7 +114,10 @@ class Pipeline(object):
 
         return len(self.src.vocab), len(self.trg.vocab)
 
-    def prepare_data(self, batch_size: int) -> None:
+    def prepare_data(self, batch_size: int, seed: Optional[int] = None) -> None:
+        # `BucketIterator` makes use of global `random` state
+        if seed:
+            random.seed(seed)
         iterators = BucketIterator.splits((self.train_data, self.valid_data, self.test_data),
                                           batch_size=batch_size, device=self.device)
         self.train_iter, self.valid_iter, self.test_iter = iterators
@@ -200,7 +204,7 @@ class Pipeline(object):
             List of tokens
         """
         with torch.no_grad():
-            sos_index = self.src.vocab.stoi[self.SOS_TOKEN]
+            sos_index = self.trg.vocab.stoi[self.SOS_TOKEN]
             eos_index = self.trg.vocab.stoi[self.EOS_TOKEN]
             unk_index = self.trg.vocab.stoi[self.UNK_TOKEN]
 
@@ -225,6 +229,6 @@ class Pipeline(object):
             winner_len = lengths[idx]
             winner = output_tensor[:winner_len, idx]
 
-            # todo: use utils.seq_to_sentence -- first figure out why not getting <unk>
+            # todo: use utils.seq_to_sentence
             result = [self.trg.vocab.itos[d] for d in winner]
             return result

cleartext/scripts/train.py
@@ -37,12 +37,12 @@ MODELS_ROOT = PROJ_ROOT / 'models'
 @click.option('--attn_units', '-a', default=100, type=int, help='Number of attention units')
 @click.option('--dropout', '-p', default=0.3, type=float, help='Dropout probability')
 @click.option('--alpha', default=0.5, type=float, help='Beam search regularization')
-@click.option('--seed', required=False, type=int, help='Random seed')
+@click.option('--seed', required=False, type=str, help='Random seed')
 def main(dataset: str,
          num_epochs: int, max_examples: Optional[int], batch_size: int,
          embed_dim: str, src_vocab: Optional[int], trg_vocab: Optional[int],
          rnn_units: int, attn_units: int,
-         dropout: float, alpha: float, seed: Optional[int] = None) -> None:
+         dropout: float, alpha: float, seed: Optional[str] = None) -> None:
     # parse/validate arguments
     if dataset.lower() == 'wikismall':
         dataset = WikiSmall
@@ -50,7 +50,9 @@ def main(dataset: str,
         dataset = WikiLarge
     else:
         raise ValueError(f'Unknown dataset "{dataset}"')
+    src_vocab = src_vocab if src_vocab else None
     trg_vocab = trg_vocab if trg_vocab else None
+    seed = int(seed) if seed else None
 
     # initialize pipeline
     pipeline = Pipeline()
@@ -71,18 +73,18 @@ def main(dataset: str,
     print()
 
     # prepare data
-    pipeline.prepare_data(batch_size)
+    pipeline.prepare_data(batch_size, seed)
 
     # build model and prepare optimizer and loss
     print('Building model')
+    if seed is not None:
+        torch.manual_seed(seed)
     trainable, total = pipeline.build_model(rnn_units, attn_units, dropout)
     print(f'Trainable parameters: {trainable} | Total parameters: {total}')
     print()
 
     # run training loop
     print(f'Training model for {num_epochs} epochs')
-    if seed and seed >= 0:
-        torch.manual_seed(seed)
     epoch = pipeline.train(num_epochs)
 
     # reload last checkpoint (without losing dataset)