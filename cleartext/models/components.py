9eaef3cb734ae8995aca53d1f2e39fb87a8f8601
Ben <bencwallace@gmail.com>
2020-06-18 19:45:20 -0400

    added documentation to model components

--- cleartext/models/components.py
+++ cleartext/models/components.py
@@ -8,12 +8,38 @@ from .. import utils
 
 
 class Encoder(nn.Module):
+    """Encoder module.
+
+    The encoder represents its inputs using fixed, pre-trained GloVe embeddings, which are passed through a
+    bidirectional GRU.
+
+    Attributes
+    ----------
+    units: int
+        Number of hidden units in the GRU.
+    embed_dim: int
+        Embedding dimensionality.
+    embedding: Module
+        Embedding module.
+    gru: Module
+        Bidirectional GRU module.
+    fc: Module
+        Fully-connected layer. Used to perform dimensionality reduction on final (bidirectional) hidden state, which
+        will be used to initialize the (unidirectional) hidden state of the decoder.
+    dropout: Module
+        Dropout module for the fully-connected layer.
+    """
     def __init__(self, embed_weights: Tensor, units: int, dropout: float) -> None:
-        """
+        """Initialize the encoder.
+
+        Constructs encoder sub-modules and initializes their weights using `utils.init_weights`.
+
         :param embed_weights: Tensor
-            Embedding weights of shape (src_vocab_size, embed_dim)
+            Embedding weights of shape (src_vocab_size, embed_dim).
         :param units: int
+            Number of GRU hidden units.
         :param dropout: float
+            Dropout probability.
         """
         super().__init__()
         self.units = units
@@ -28,11 +54,12 @@ class Encoder(nn.Module):
         utils.init_weights_(self.fc)
 
     def forward(self, source: Tensor) -> Tuple[Tensor, Tensor]:
-        """
+        """Encoder a source sequence by forward propagation.
+
         :param source: Tensor
             Source sequence of shape (seq_len, batch_size)
         :return: Tuple[Tensor, Tensor]
-            Outputs of shape (seq_len, batch_size, units) and state of shape (batch_size, 2 * units)
+            Outputs of shape (seq_len, batch_size, units) and state of shape (batch_size, units)
         """
         embedded = self.embedding(source)
         outputs, state = self.gru(embedded)
@@ -46,12 +73,32 @@ class Encoder(nn.Module):
 
 
 class Attention(nn.Module):
+    """Attention module.
+
+    Used to compute Bahdanau attention weights over encoder outputs.
+
+    Attributes
+    ----------
+    attn_in: int
+        Dimensionality of attention input.
+    fc: Module
+        First fully-connected layer
+    fc2: Module
+        Second fully-connected layer.
+    dropout: Module
+        Dropout module
+    """
     def __init__(self, enc_units: int, dec_units: int, units: int, dropout: float = 0) -> None:
-        """
+        """Initializes the attention module.
+
         :param enc_units: int
+            Encoder output dimensionality.
         :param dec_units: int
+            Decoder state dimensionality.
         :param units: int
+            Attention dimensionality.
         :param dropout: float
+            Dropout probability.
         """
         super().__init__()
         self.attn_in = 2 * enc_units + dec_units
@@ -64,13 +111,14 @@ class Attention(nn.Module):
         utils.init_weights_(self.fc2)
 
     def forward(self, dec_state: Tensor, enc_outputs: Tensor) -> Tensor:
-        """
+        """Computes (Bahdanau) attention weights.
+
         :param dec_state: Tensor
-            Previous or initial decoder state of shape (batch_size, dec_units)
+            Previous or initial decoder state of shape (batch_size, dec_units).
         :param enc_outputs: Tensor
-            Encoder outputs of shape (source_len, batch_size, 2 * enc_units)
+            Encoder outputs of shape (source_len, batch_size, 2 * enc_units).
         :return:
-            Attention weights of shape (batch_size, source_len)
+            Attention weights of shape (batch_size, source_len).
         """
         source_len = enc_outputs.shape[0]
 
@@ -88,12 +136,34 @@ class Attention(nn.Module):
 
 
 class Decoder(nn.Module):
+    """Decoder module.
+
+    Predicts next token probabilities using previous token and state and context vector.
+
+    Attributes
+    ----------
+    vocab_size: int
+        Target vocabulary size.
+    embed_dim: int
+        Embedding dimension.
+    embedding: Module
+        Embedding layer.
+    rnn: Module
+        GRU.
+    fc: Module
+        Fully-connected layer that outputs scores over target vocabulary.
+    dropout: Module
+        Dropout module.
+    """
     def __init__(self, embed_weights: Tensor, units: int, dropout: float, enc_units: int) -> None:
-        """
+        """Initializes the decoder module.
+
         :param embed_weights: Tensor
-            Embedding weights of shape (trg_vocab_size, embed_dim)
+            Embedding weights of shape (trg_vocab_size, embed_dim).
         :param units: int
+            Number of hidden units in the GRU.
         :param dropout: float
+            Dropout probability.
         """
         super().__init__()
         self.vocab_size, embed_dim = embed_weights.shape
@@ -107,15 +177,16 @@ class Decoder(nn.Module):
         utils.init_weights_(self.fc)
 
     def forward(self, token: Tensor, dec_state: Tensor, context: Tensor) -> Tuple[Tensor, Tensor]:
-        """
+        """Decodes the next token.
+
         :param token: Tensor
-            Token of shape (batch_size,)
+            Token of shape (batch_size,).
         :param dec_state:
-            Decoder state of shape (batch_size, dec_units)
+            Decoder state of shape (batch_size, dec_units).
         :param context:
-            Context vector of shape (1, batch_size, 2 * enc_units)
+            Context vector of shape (1, batch_size, 2 * enc_units).
         :return: Tuple[Tensor, Tensor]
-            Vocabulary scores of shape (batch_size, vocab_size) and decoder state of shape (batch_size, dec_units)
+            Vocabulary scores of shape (batch_size, vocab_size) and decoder state of shape (batch_size, dec_units).
         """
         token = token.unsqueeze(0)
         embedded = self.embedding(token)
