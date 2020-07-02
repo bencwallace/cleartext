56da0f30a36fade442d4410711501a14be131d54
Ben <bencwallace@gmail.com>
2020-07-02 16:13:42 -0400

    removed white space before bleu computation

--- cleartext/pipeline/pipeline.py
+++ cleartext/pipeline/pipeline.py
@@ -281,6 +281,8 @@ class Pipeline(object):
         outputs = [self.beam_search(s, beam_size, max_len, alpha) for s in sources]
 
         # Compute (test) BLEU score
+        targets = [' '.join(target).split() for target in targets]
+        outputs = [' '.join(output).split() for output in outputs]
         bleu = bleu_score(targets, outputs)
 
         return train_loss, valid_loss, test_loss, bleu
